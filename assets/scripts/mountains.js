a1ee70a006d19db295154533ad0b4c11beca9b90
JQ <jesusq+github@gmail.com>
2022-06-29 09:42:47 -0700

    Implement mountains functionality

--- assets/scripts/mountains.js
+++ assets/scripts/mountains.js
@@ -1,1 +1,33 @@
 console.log("--mountains.js--")
+
+let mountainsDropdown = document.querySelector("#mountainsDropdown");
+let mtnName = document.querySelector("#name");
+let mtnDesc = document.querySelector("#desc");
+let mtnElevation = document.querySelector("#elevation");
+let mtnEffort = document.querySelector("#effort");
+let mtnInfo = document.querySelector("#info");
+
+setTimeout(() => {
+    // console.log("hi");
+    // console.log(mountainsArray);
+
+    mountainsArray.forEach((mountain) => {
+        mountainsDropdown.innerHTML += `<option value="${mountain.name}">${mountain.name}</option>`
+    });
+}, 100);
+
+// load single mountain info based on dropdown selection
+mountainsDropdown.addEventListener("change", function(event) {
+    console.log(event.target.value);
+
+    const foundMtn = mountainsArray.find(mountain => mountain.name === event.target.value);
+
+    console.log(foundMtn);
+
+    mtnName.innerHTML = foundMtn.name;
+    mtnDesc.innerHTML = foundMtn.desc;
+    mtnElevation.innerHTML = foundMtn.elevation;
+    mtnEffort.innerHTML = foundMtn.effort;
+
+    mtnInfo.classList.remove("d-none");
+});
