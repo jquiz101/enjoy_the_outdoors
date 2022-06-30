307180361f17b063a2f27f1c8bc4fe25d9e5b843
JQ <jesusq+github@gmail.com>
2022-06-30 08:56:34 -0700

    Do stuff after mountainsArray is populated

--- assets/scripts/load_mountains_data.js
+++ assets/scripts/load_mountains_data.js
@@ -6,6 +6,9 @@ window.onload = function(){
 
     loadJsonData("assets/data/mountains.json").then((mountains) => {
         mountainsArray = mountains.mountains;
+
+        // put code here that depends on the mountainsArray being populated
+        // generateMountainsDDL(mountainsArray);
     })
 
 }
@@ -15,4 +18,12 @@ let loadJsonData = async (path) => {
     let response = await fetch(path)
     let data = await response.json()
     return data
+}
+
+// hoisting - functions are accessible even if they're defined after where they're called/used
+function generateMountainsDDL(arrayOfData) {
+    console.log("generateMountainsDDL()");
+    console.log(arrayOfData);
+
+    // possibly create mountains dropdown options here
 }
